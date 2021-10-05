2ea04f7ddf6a17a20737aaeed1fcf6c77b65e246
Megalon <megalongaming@gmail.com>
2021-10-05 16:26:13 -0700

    Version bump

--- NickMirrorMatchMod/PluginInfo.cs
+++ NickMirrorMatchMod/PluginInfo.cs
@@ -8,6 +8,6 @@ namespace NickMirrorMatchMod
     {
         public const string PLUGIN_GUID = "megalon.nick_mirror_match_mod";
         public const string PLUGIN_NAME = "NickMirrorMatchMod";
-        public const string PLUGIN_VERSION = "1.0.0";
+        public const string PLUGIN_VERSION = "1.0.1";
     }
 }
