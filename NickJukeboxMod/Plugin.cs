6f4fde0372128ca2c589debe4c477daf4084a1f1
Cyprian Bergonia <2168960-nikoku@users.noreply.gitlab.com>
2021-10-06 13:48:35 -0500

    rename files

---
+++ NickJukeboxMod/Plugin.cs
@@ -0,0 +1,44 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Reflection;
+
+namespace NickJukeboxMod
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static Plugin Instance;
+
+        private void Awake()
+        {
+            Logger.LogDebug($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");
+
+            if (Instance)
+            {
+                DestroyImmediate(this);
+                return;
+            }
+            Instance = this;
+
+            var config = this.Config;
+
+            config.SettingChanged += OnConfigSettingChanged;
+
+            // Harmony patches
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        static void OnConfigSettingChanged(object sender, EventArgs args)
+        {
+            LogDebug($"{PluginInfo.PLUGIN_NAME} OnConfigSettingChanged");
+            Plugin.Instance?.Config?.Reload();
+        }
+
+        internal static void LogDebug(string message) => Instance.Log(message, LogLevel.Debug);
+        internal static void LogInfo(string message) => Instance.Log(message, LogLevel.Info);
+        internal static void LogError(string message) => Instance.Log(message, LogLevel.Error);
+        private void Log(string message, LogLevel logLevel) => Logger.Log(logLevel, message);
+    }
+}
---
+++ NickJukeboxMod/PluginInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace NickJukeboxMod
+{
+    class PluginInfo
+    {
+        public const string PLUGIN_GUID = "orangeswim.nick_jukebox_mod";
+        public const string PLUGIN_NAME = "NickJukeboxMod";
+        public const string PLUGIN_VERSION = "1.0.";
+    }
+}
